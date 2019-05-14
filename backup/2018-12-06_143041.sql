b37bb85c10214447a899b36223dfe699facdd681
ahmadmahdar16 <ahmadmahdar16@users.noreply.github.com>
2019-05-15 02:33:07 +0700

    Add files via upload

---
+++ backup/2018-12-06_143041.sql
@@ -0,0 +1,130 @@
+DROP TABLE tbl_disposisi;
+
+CREATE TABLE `tbl_disposisi` (
+  `id_disposisi` int(10) NOT NULL AUTO_INCREMENT,
+  `tujuan` varchar(250) NOT NULL,
+  `isi_disposisi` mediumtext NOT NULL,
+  `sifat` varchar(100) NOT NULL,
+  `batas_waktu` date NOT NULL,
+  `catatan` varchar(250) NOT NULL,
+  `id_surat` int(10) NOT NULL,
+  `id_user` tinyint(2) NOT NULL,
+  PRIMARY KEY (`id_disposisi`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+INSERT INTO tbl_disposisi VALUES("1","Tasikmalaya","WASD","Rahasia","2018-12-20","TARRR","1","1");
+
+
+
+DROP TABLE tbl_instansi;
+
+CREATE TABLE `tbl_instansi` (
+  `id_instansi` tinyint(1) NOT NULL,
+  `institusi` varchar(150) NOT NULL,
+  `nama` varchar(150) NOT NULL,
+  `status` varchar(150) NOT NULL,
+  `alamat` varchar(150) NOT NULL,
+  `kacab` varchar(50) NOT NULL,
+  `nip` varchar(25) NOT NULL,
+  `website` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `logo` varchar(250) NOT NULL,
+  `id_user` tinyint(2) NOT NULL,
+  PRIMARY KEY (`id_instansi`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO tbl_instansi VALUES("1","PT. Bank Tabungan Negara Persero TBK","Bank Tabungan Negara Cab.Tasikmalaya","Aktif","Jalan Sutisna Senjaya No.101 Cikalang, Tawang, Tasikmalaya, Jawa Barat 46112","Yudha Andaka","5406","http://www.btn.co.id","kc.tasikmalaya@btn.co.id","logo.png","1");
+
+
+
+DROP TABLE tbl_klasifikasi;
+
+CREATE TABLE `tbl_klasifikasi` (
+  `id_klasifikasi` int(5) NOT NULL AUTO_INCREMENT,
+  `unit` varchar(30) NOT NULL,
+  `nama` varchar(250) NOT NULL,
+  `uraian` mediumtext NOT NULL,
+  `id_user` tinyint(2) NOT NULL,
+  PRIMARY KEY (`id_klasifikasi`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+
+DROP TABLE tbl_sett;
+
+CREATE TABLE `tbl_sett` (
+  `id_sett` tinyint(1) NOT NULL,
+  `surat_masuk` tinyint(2) NOT NULL,
+  `surat_keluar` tinyint(2) NOT NULL,
+  `referensi` tinyint(2) NOT NULL,
+  `id_user` tinyint(2) NOT NULL,
+  PRIMARY KEY (`id_sett`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO tbl_sett VALUES("1","10","10","10","1");
+
+
+
+DROP TABLE tbl_surat_keluar;
+
+CREATE TABLE `tbl_surat_keluar` (
+  `id_surat` int(10) NOT NULL AUTO_INCREMENT,
+  `no_agenda` int(10) NOT NULL,
+  `tujuan` varchar(250) NOT NULL,
+  `no_surat` varchar(50) NOT NULL,
+  `isi` mediumtext NOT NULL,
+  `unit` varchar(30) NOT NULL,
+  `tgl_surat` date NOT NULL,
+  `tgl_catat` date NOT NULL,
+  `file` varchar(250) NOT NULL,
+  `keterangan` varchar(250) NOT NULL,
+  `id_user` tinyint(2) NOT NULL,
+  PRIMARY KEY (`id_surat`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+INSERT INTO tbl_surat_keluar VALUES("1","1","3333","4444","6666","2222","2018-12-04","2018-12-03","","5555","1");
+
+
+
+DROP TABLE tbl_surat_masuk;
+
+CREATE TABLE `tbl_surat_masuk` (
+  `id_surat` int(10) NOT NULL AUTO_INCREMENT,
+  `no_agenda` int(10) NOT NULL,
+  `no_surat` varchar(50) NOT NULL,
+  `asal_surat` varchar(250) NOT NULL,
+  `isi` mediumtext NOT NULL,
+  `unit` varchar(30) NOT NULL,
+  `indeks` varchar(30) NOT NULL,
+  `tgl_surat` date NOT NULL,
+  `tgl_diterima` date NOT NULL,
+  `file` varchar(250) NOT NULL,
+  `keterangan` varchar(250) NOT NULL,
+  `id_user` tinyint(2) NOT NULL,
+  PRIMARY KEY (`id_surat`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+INSERT INTO tbl_surat_masuk VALUES("1","1","100/SPM/2017","Bekasi","iopuio","CCR","op","2018-11-28","2018-12-03","4077-GAMBAR.jpg","wasd","1");
+
+
+
+DROP TABLE tbl_user;
+
+CREATE TABLE `tbl_user` (
+  `id_user` tinyint(2) NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(35) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `nip` varchar(25) NOT NULL,
+  `admin` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+INSERT INTO tbl_user VALUES("1","admin","0192023a7bbd73250516f069df18b500","Sekretaris","157006122","1");
+INSERT INTO tbl_user VALUES("2","157006122","dec4d8f1c17a6b50fb98a44b6ab11658","mahdar","157006122","2");
+INSERT INTO tbl_user VALUES("3","123456","e10adc3949ba59abbe56e057f20f883e","kiki","123456","3");
+INSERT INTO tbl_user VALUES("4","rosana08","7e5ed0f27edd819fe4a09b1aa7a4c3b5","Rosana","15984","2");
+
+
+
